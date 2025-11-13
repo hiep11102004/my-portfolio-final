38c218cb5bbb70dda7d01111906e32c964e672a0
hiep11102004 <hiep1110nd@gmail.com>
2025-11-13 17:22:08 +0700

    Last Update Project: Personalize porfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -60,7 +60,7 @@ document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
             document.title = "Portfolio | Ngoc Hiep";
-            $("#favicon").attr("href", "assets/images/favicon.png");
+            $("#favicon").attr("href", "./assets/images/Hiệp/avt.jpg");
         }
         else {
             document.title = "Come Back To Portfolio";
